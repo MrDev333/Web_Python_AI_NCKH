ce6c3998efab50a9aaef8613feb6407bf5dc5205
An Khang <nguyenankhangpro7895123@gmail.com>
2025-09-27 22:59:25 +0700

    Add readme, create new django structure v1.6

--- Web_AI/urls.py
+++ Web_AI/urls.py
@@ -2,11 +2,5 @@ from django.urls import path, include
 from Web_AI import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
     path("scanner/", views.scanner, name="scanner"),
-    path("profile/", views.profile_view, name="profile"),
-    path("settings/", views.settings_view, name="settings"),
-    path("activate/", views.activate, name="activate"),
 ]
